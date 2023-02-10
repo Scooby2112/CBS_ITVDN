b333a2c5c6079477657c12f4ae322eda1ba4d1f0
Volodymyr Plemennyk <vplemennyk@gmail.com>
2023-02-10 19:53:29 +0200

    Added lesson3

---
+++ src/main/Java_4_Advanced/Lesson3/Task2/Main.java
@@ -0,0 +1,14 @@
+package main.Java_4_Advanced.Lesson3.Task2;
+
+public class Main {
+    public static void main(String[] args) {
+        String sentences = "Импала, также называемая Деткой — автомобиль Дина торговой марки Chevrolet Impala, переданный ему его отцом Джоном.";
+
+        String firstPart = sentences.substring(0, sentences.length() / 2 + 1);
+        String secondPart = sentences.substring(sentences.length() / 2 + 1);
+        System.out.println(firstPart);
+        System.out.println(secondPart);
+
+
+    }
+}
