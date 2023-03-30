2b7db164c94224059b9f693593ce4a9bf65c691e
Volodymyr Plemennyk <vplemennyk@gmail.com>
2023-03-30 22:51:56 +0300

    Updated lesson 4 - JDBC & Hibernate

--- src/main/java/Java_JDBC_Hibernate/Lesson4/Task2/Main.java
+++ src/main/java/Java_JDBC_Hibernate/Lesson4/Task2/Main.java
@@ -1,14 +1,26 @@
 package Java_JDBC_Hibernate.Lesson4.Task2;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         AnimalRepository ar = new AnimalRepository();
         Animal animal = new Animal();
 
-        animal.setAge(10);
-        animal.setName("Nik");
-        animal.setTail(true);
+//        animal.setAge(15);
+//        animal.setName("Tomas");
+//        animal.setTail(false);
+//
+//        ar.add(animal);
 
-        ar.add(animal);
+//        List<Animal> animalList = ar.getAnimalList();
+//
+//        for (Animal animal1 : animalList){
+//            System.out.println(animal1.getId() + " " + animal1.getName() + " " + animal1.getAge());
+//    }
+
+//        ar.remove(animal);
+
+        ar.getByID(3);
     }
 }
